96c3f1a1e7cb51015c74f0db8b27771a11a2cee4
unknown <cindy.mauricioe@gmail.com>
2023-09-21 12:17:13 -0300

    seccion de marcas terminado

---
+++ js/marcas.js
@@ -0,0 +1,7 @@
+// const containerImg = document.querySelectorAll(".container-img");
+
+// containerImg.forEach((elemento) => {
+//     if (!elemento.getAttribute("data-aos")) {
+//         elemento.setAttribute("data-aos", "fade-up");
+//     }
+// });
